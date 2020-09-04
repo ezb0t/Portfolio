8e3227655851c578d1acf88ec069a11bc5c15a22
ezb0t <alan.sato96@outlook.com>
2020-09-04 19:06:08 -0300

    Página de contato

---
+++ Projeto_12/pages/contato.php
@@ -0,0 +1,17 @@
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.471673081673!2d-46.659822284453426!3d-23.587411268441258!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59f1069d11d1%3A0xcb936109af9ce541!2sParque%20Ibirapuera!5e0!3m2!1spt-BR!2sbr!4v1599185467306!5m2!1spt-BR!2sbr" width="100%" height="420px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div><!--map-->
+<div class="contato-container">
+    <div class="center">
+        <form>
+            <input type="text" name="nome" placeholder="Nome" id="">
+            <div></div>
+            <input type="email" name="email" placeholder="E-mail" id="">
+            <div></div>
+            <input type="text" name="tel" placeholder="Telefone" id="">
+            <div></div>
+            <textarea name="mensagem" placeholder="Mensagem" id=""></textarea>
+            <div></div>
+            <input type="submit" name="acao" value="Enviar">
+        </form>
+    </div><!--center-->
+    <div class="clear"></div><!--clear-->
+</div><!--contato-container-->
