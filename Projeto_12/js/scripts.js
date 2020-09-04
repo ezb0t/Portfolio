3343d38a0898d7a8bd8fc0856866e9c82067addb
ezb0t <alan.sato96@outlook.com>
2020-09-04 19:05:39 -0300

    Alterações básicas

--- Projeto_12/js/scripts.js
+++ Projeto_12/js/scripts.js
@@ -1,6 +1,6 @@
 $(function(){
 	$('nav.mobile').click(function(){
-		var listaMenu = $('nav.mobile ul');
+		var listaMenu = $('nav.mobile ul')
 		//fadeIn
 		/*if(listaMenu.is(':hidden') == true){
 			listaMenu.fadeIn();
@@ -8,16 +8,34 @@ $(function(){
 			listaMenu.fadeOut();
 		}*/
 
-		//slideToggle
-		listaMenu.slideToggle();
-
 		//Sem efeitos
 		/*if(listaMenu.is(':hidden') == true){
-			//listaMenu.show();
+			listaMenu.show();
 			listaMenu.css('display','block');
 		}else{
-			//listaMenu.hide();
+			listaMenu.hide();
 			listaMenu.css('display','none');
 		}*/
+
+		//slideToggle
+		if(listaMenu.is(':hidden') == true){
+			var icone = $('.btn-mobile').find('i')
+			icone.removeClass('fa-bars')
+			icone.addClass('fa-times')
+			listaMenu.slideToggle()
+		}else{
+			var icone = $('.btn-mobile').find('i')
+			icone.removeClass('fa-times')
+			icone.addClass('fa-bars')
+			listaMenu.slideToggle()
+		}
 	})
+
+	//Scroll Dinamico
+
+	if($('target').length > 0){
+		var elemento = '#'+$('target').attr('target')
+		var divScroll = $(elemento).offset().top
+		$('html,body').animate({'scrollTop':divScroll},1000)
+	}
 })
