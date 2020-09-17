ab24f2bbff7d6afc6979706104e6eaebf1f0a12a
ezb0t <alan.sato96@outlook.com>
2020-09-17 19:30:30 -0300

    Novas funções

--- Projeto_12/config.php
+++ Projeto_12/config.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+    date_default_timezone_set('America/Sao_Paulo');
     $autoload = function($class){
         if($class == 'Email'){
             require_once('classes/phpmailer/PHPMailerAutoload.php');
@@ -6,5 +8,47 @@
         include('classes/'.$class.'.php');
     };
     spl_autoload_register($autoload);
+
     define('INCLUDE_PATH','http://localhost/Danki-Code/Projetos/Projeto_12/');
+    define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
+
+    define('BASE_DIR_PAINEL',__DIR__.'/painel');
+
+    //Conectar com banco de dados!
+    define('HOST','localhost');
+    define('USER','root');
+    define('PASSWORD','');
+    define('DATABASE','projeto_12');
+
+    //Constantes para o painel de controle
+	define('NOME_EMPRESA','Nome da sua empresa');
+
+    //Funções
+    function pegaCargo($indice){
+		return Painel::$cargos[$indice];
+	}
+    
+    function selecionadoMenu($par){
+		$url = explode('/',@$_GET['url'])[0];
+		if($url == $par){
+			echo 'class="menu-active"';
+		}
+    }
+    
+    function verificaPermissaoMenu($permissao){
+		if($_SESSION['cargo'] >= $permissao){
+			return;
+		}else{
+			echo 'style="display:none;"';
+		}
+    }
+    
+    function verificaPermissaoPagina($permissao){
+		if($_SESSION['cargo'] >= $permissao){
+			return;
+		}else{
+			include('painel/pages/permissao_negada.php');
+			die();
+		}
+	}
 ?>
