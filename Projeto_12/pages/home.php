8d3ffeeb744620894a6e8050ab0cbd3eef0fa689
ezb0t <alan.sato96@outlook.com>
2020-09-17 19:33:37 -0300

    Atualização home

--- Projeto_12/pages/home.php
+++ Projeto_12/pages/home.php
@@ -1,82 +1,86 @@
 <section class="banner-container">
-    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-form.png')" class="banner-single"></div><!--banner-single-->
+	<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-form.jpg')" class="banner-single"></div><!--banner-single-->
     <div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-form2.jpg')" class="banner-single"></div><!--banner-single-->
-    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-form3.jpeg')" class="banner-single"></div><!--banner-single-->
-    <div class="overlay"></div><!--overlay-->
-    <div class="center">
-        <form method="post">
-            <h2>Qual seu e-mail?</h2>
-            <input type="email" name="email" required>
-            <input type="hidden" name="identificador" value="form_home">
-            <input type="submit" name="acao" value="Enviar!">
-        </form>
-    </div><!--center-->
-    <div class="bullets"></div><!--bullets-->
-</section><!--banner-containerl-->
+    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-form3.jpg')" class="banner-single"></div><!--banner-single-->
+	<div class="overlay"></div><!--overlay-->
+	<div class="center">
+		<form method="post">
+			<h2>Qual o seu melhor e-mail?</h2>
+			<input type="email" name="email" required />
+			<input type="hidden" name="identificador" value="form_home" />
+			<input type="submit" name="acao" value="Cadastrar!">
+		</form>
+	</div><!--center-->
+	<div class="bullets"></div><!--bullets-->
+</section><!--banner-principal-->
 
 <section class="descricao-autor">
-    <div class="center">
-        <div id="sobre" class="w50 left">
-            <h2>Alan Sato</h2>
-            <p>Etiam nibh amet vel erat metus mattis curabitur lobortis fringilla nam cubilia, hendrerit potenti ut ligula aliquam libero dui orci pharetra praesent, eros erat at dictumst primis libero aptent lectus eros varius. arcu volutpat pharetra aliquam arcu porttitor sed fusce class arcu, cursus quisque mattis luctus donec enim quisque suscipit, nunc gravida placerat bibendum quisque mattis phasellus sociosqu. dictumst imperdiet est cursus ipsum massa facilisis scelerisque class, massa commodo aliquet libero quam litora laoreet ullamcorper, placerat justo sit mollis class ipsum augue.</p>
-            <p>Tellus scelerisque hac tempus ante fusce bibendum facilisis inceptos sodales venenatis nulla at quam eleifend adipiscing, ornare justo id lobortis blandit aliquet habitasse duis ante interdum aptent platea suspendisse. fermentum dapibus phasellus mauris dolor est vitae, conubia neque fames tristique tellus dui, dapibus etiam mi leo interdum. nulla metus lectus proin fusce ipsum lacus ultrices et justo aliquam donec ultricies, turpis lacus accumsan fringilla tempor accumsan auctor curae tellus magna suscipit. aliquam fringilla vulputate mollis sociosqu amet ullamcorper elit, diam phasellus donec pretium quam id tristique, ornare dui quisque erat fermentum eget.</p>
-        </div><!--w50 left-->
-        <div class="w50 left">
-            <img class="right" src="<?php echo INCLUDE_PATH; ?>images/cinza.jpg">
-        </div><!--w50 left-->
-        <div class="clear"></div><!--clear-->
-    </div><!--center-->
+	<div class="center">
+		<div class="w50 left">
+			<h2><?php echo $infoSite['nome_autor']; ?></h2>
+			<p><?php echo $infoSite['descricao']; ?></p>
+		</div><!--w50-->
+		<div class="w50 left">
+			<!--Pegar imagem depois-->
+			<img class="right" src="<?php echo INCLUDE_PATH; ?>images/foto.jpg" />
+		</div><!--w50-->
+		<div class="clear"></div><!--clear-->
+	</div><!--center-->
 </section><!--descricao-autor-->
 
 <section class="especialidades">
-    <div class="center">
-        <h2 class="title">Especialidades</h2><!--title-->
-        <div class="w33 left box-especialidade">
-            <h3 style="color: #F06428"><i class="fab fa-html5"></i></h3>
-            <h4>HTML5</h4>
-            <p>Curabitur tristique iaculis scelerisque fermentum cubilia integer pretium eros vulputate, aenean semper aenean vulputate volutpat sed lacinia fermentum, lacinia donec accumsan id aliquet aptent hac conubia.</p>
-        </div><!--w33 left box-especialidade-->
-        <div class="w33 left box-especialidade">
-            <h3 style="color: #0062B0"><i class="fab fa-css3-alt"></i></h3>
-            <h4>CSS3</h4>
-            <p>Magna malesuada commodo metus tempus nostra consectetur massa per blandit, turpis vehicula nostra convallis facilisis velit neque fames, ultrices malesuada est mattis integer imperdiet etiam felis.</p>
-        </div><!--w33 left box-especialidade-->
-        <div class="w33 left box-especialidade">
-            <h3 style="color: #D6BE00"><i class="fab fa-js-square"></i></h3>
-            <h4>JavaScript</h4>
-            <p>Curae auctor curae porta mauris congue proin nam aliquam tristique, aenean id primis at viverra rhoncus litora mi, vitae id lacinia proin augue euismod amet consectetur.</p>
-        </div><!--w33 left box-especialidade-->
-        <div class="clear"></><!--clear-->
-    </div><!--center-->
+	<div class="center">
+		<h2 class="title">Especialidades</h2>
+		<div class="w33 left box-especialidade">
+			<h3><i class="<?php echo $infoSite['icone1']; ?>" aria-hidden="true"></i></h3>
+			<h4>CSS3</h4>
+			<p><?php echo $infoSite['descricao1']; ?></p>
+		</div><!--box-especialidade-->
+		<div class="w33 left box-especialidade">
+			<h3><i class="<?php echo $infoSite['icone2']; ?>" aria-hidden="true"></i></h3>
+			<h4>HTML5</h4>
+			<p><?php echo $infoSite['descricao2']; ?></p>
+		</div><!--box-especialidade-->
+		<div class="w33 left box-especialidade">
+			<h3><i class="<?php echo $infoSite['icone3']; ?>" aria-hidden="true"></i></h3>
+			<h4>JavaScript</h4>
+			<p><?php echo $infoSite['descricao3']; ?></p>
+		</div><!--box-especialidade-->
+		<div class="clear"></div><!--clear-->
+	</div><!--center-->
 </section><!--especialidades-->
 
 <section class="extras">
-    <div class="center">
-        <div id="depoimentos" class="w50 left depoimentos-container">
-            <h2 class="title">Depoimentos dos nossos clientes</h2><!--title-->
-            <div class="depoimento-single">
-                <p class="depoimento-descricao">“Pharetra etiam gravida et laoreet porta sollicitudin auctor, purus per nostra habitant venenatis platea fermentum scelerisque, ultrices viverra euismod dolor sodales nunc. class sagittis adipiscing ad posuere, cursus dui semper, est etiam aliquam.”</p>
-                <p class="nome-autor">Marcos Nunes</p><!--nome-autor-->
-            </div><!--depoimento-single-->
-            <div class="depoimento-single">
-                <p class="depoimento-descricao">“Euismod rutrum vulputate malesuada condimentum volutpat curabitur sapien purus massa, phasellus aenean tempor congue torquent aenean curae. sit convallis dapibus egestas at id, bibendum condimentum nulla pharetra ultrices risus, sem suscipit fringilla fames.”</p>
-                <p class="nome-autor">Alessandra Moraes</p><!--nome-autor-->
-            </div><!--depoimento-single-->
-            <div class="depoimento-single">
-                <p class="depoimento-descricao">“Duis luctus faucibus quis varius fames amet cras duis eros justo, aliquet mattis luctus purus odio aenean ut elementum fermentum, ornare euismod faucibus aliquet consectetur potenti tellus netus maecenas. netus accumsan risus, pellentesque.”</p>
-                <p class="nome-autor">Isabelly Viana</p><!--nome-autor-->
-            </div><!--depoimento-single-->
-        </div><!--w50 left depoimentos-container-->
-        <div id="servicos" class="w50 left servicos-container">
-            <h2 class="title">Serviços</h2><!--title-->
-            <div class="servicos">
-                <ul>
-                    <li>Elit nostra condimentum etiam vivamus faucibus aptent sagittis semper lorem purus, eu a sodales neque ad vivamus egestas maecenas odio vulputate, class sodales augue ullamcorper mattis arcu tincidunt pretium felis. etiam vivamus nibh libero duis convallis nisl vitae purus, turpis urna dapibus primis enim porttitor.</li>
-                    <li>Hac aliquam arcu ligula mattis nullam interdum integer, adipiscing sit nec interdum malesuada nam, tempor potenti facilisis purus quam nunc. hendrerit fames euismod in senectus luctus sagittis nisi, magna gravida porttitor phasellus ante habitasse, fusce nostra potenti odio volutpat nulla.</li>
-                    <li>Magna tortor euismod molestie aliquam mi condimentum senectus, ac morbi nisi posuere potenti tincidunt. elementum aptent egestas condimentum tellus vivamus blandit torquent diam, eu varius amet enim nulla sed consequat aliquet, fermentum dictumst enim luctus pellentesque vehicula elit. ornare nibh habitant morbi mauris malesuada, ut ultrices taciti elementum, nec metus phasellus ullamcorper.</li>
-                </ul>
-            </div><!--servicos-->
-        </div><!--w50-->
-        <div class="clear"></div><!--clear-->
-    </div><!--center-->
+	<div class="center">
+		<div id="depoimentos" class="w50 left depoimentos-container">
+			<h2 class="title">Depoimentos dos nossos clientes</h2>
+			<?php
+				$sql = MySql::conectar()->prepare("SELECT * FROM `tb_site.depoimentos` ORDER BY order_id ASC LIMIT 3");
+				$sql->execute();
+				$depoimentos = $sql->fetchAll();
+				foreach ($depoimentos as $key => $value) {
+			?>
+			<div class="depoimento-single">
+				<p class="depoimento-descricao">"<?php echo $value['depoimento']; ?>"</p>
+				<p class="nome-autor"><?php echo $value['nome']; ?> - <?php echo $value['data']; ?></p>
+			</div><!--depoimento-single-->
+			<?php } ?>
+		</div><!--w50-->
+		<div id="servicos" class="w50 left servicos-container">
+			<h2 class="title">Serviços</h2>
+			<div class="servicos">
+			<ul>
+				<?php
+				$sql = MySql::conectar()->prepare("SELECT * FROM `tb_site.servicos` ORDER BY order_id ASC LIMIT 3");
+				$sql->execute();
+				$servicos = $sql->fetchAll();
+				foreach ($servicos as $key => $value) {
+				?>
+				<li><?php echo $value['servico']; ?></li>
+				<?php } ?>
+			</ul>
+			</div><!--servicos-->
+		</div><!--w50-->
+		<div class="clear"></div><!--clear-->
+	</div><!--center-->
 </section><!--extras-->
