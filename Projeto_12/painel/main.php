4c039206115f4d20c0f44cc6a62f4f9ed41eeeb9
ezb0t <alan.sato96@outlook.com>
2020-09-17 19:33:30 -0300

    Atualização main

--- Projeto_12/painel/main.php
+++ Projeto_12/painel/main.php
@@ -16,23 +16,40 @@
     </head>
     <body>
         <div class="menu">
-            <div class="box-usuario">
-                <?php
-			        if($_SESSION['img'] == ''){
-		        ?>
-                <div class="avatar-usuario">
-                    <i class="fa fa-user"></i>
-                </div><!--avatar-usuario-->
-                <?php }else{ ?>
-                    <div class="imagem-usuario">
-                        <img src="<?php echo INCLUDE_PATH_PAINEL ?>uploads/<?php echo $_SESSION['img']; ?>">
+            <div class="menu-wraper">
+                <div class="box-usuario">
+                    <?php
+                        if($_SESSION['img'] == ''){
+                    ?>
+                    <div class="avatar-usuario">
+                        <i class="fa fa-user"></i>
                     </div><!--avatar-usuario-->
-                <?php } ?>
-                <div class="nome-usuario">
-                    <p><?php echo $_SESSION['nome']; ?></p>
-                    <p><?php echo pegaCargo($_SESSION['cargo']); ?></p>
-                </div><!--nome-usuario-->
-            </div><!--box-usuario-->    
+                    <?php }else{ ?>
+                        <div class="imagem-usuario">
+                            <img src="<?php echo INCLUDE_PATH_PAINEL ?>uploads/<?php echo $_SESSION['img']; ?>">
+                        </div><!--avatar-usuario-->
+                    <?php } ?>
+                    <div class="nome-usuario">
+                        <p><?php echo $_SESSION['nome']; ?></p>
+                        <p><?php echo pegaCargo($_SESSION['cargo']); ?></p>
+                    </div><!--nome-usuario-->
+                </div><!--box-usuario--> 
+                <div class="items-menu">
+                    <h2>Cadastro</h2>
+                    <a href="<?php echo INCLUDE_PATH_PAINEL ?>cadastrar-depoimento">Cadastrar Depoimentos</a>
+                    <a href="">Cadastrar Serviço</a>
+                    <a href="">Cadastrar Slides</a>
+                    <h2>Gestão</h2>
+                    <a href="">Listar Depoimento</a>
+                    <a href="">Listar Serviço</a>
+                    <a href="">Listar Slides</a>
+                    <h2>Administração do Painel</h2>
+                    <a href="<?php echo INCLUDE_PATH_PAINEL ?>editar-usuario">Editar Usuário</a>
+                    <a href="">Adicionar Usuários</a>
+                    <h2>Configuração Geral</h2>
+                    <a href="">Editar</a>
+                </div><!--items-menu-->
+            </div><!--menu-wraper-->   
         </div><!--menu-->
 
         <header>
@@ -41,11 +58,18 @@
                     <i class="fas fa-bars"></i>
                 </div><!--menu-btn-->
                 <div class="logout">
+                    <a href="<?php echo INCLUDE_PATH_PAINEL ?>"><i class="fas fa-home"></i> <span>Página Inicial</span></a>
                     <a href="<?php echo INCLUDE_PATH_PAINEL ?>?logout"><i class="fas fa-sign-out-alt"></i> <span>Sair</span></a>
                 </div><!--logout-->
                 <div class="clear"></div><!--clear-->
             </div><!--center-->
         </header>
+        
+        <section class="content">
+            <?php Painel::carregarPagina(); ?>
+        </section><!--content-->
 
+        <script src="<?php echo INCLUDE_PATH ?>js/jquery.js"></script>
+        <script src="<?php echo INCLUDE_PATH_PAINEL ?>js/main.js"></script>
     </body>
 </html>
